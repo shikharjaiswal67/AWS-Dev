58f91c8a7cb66db5befec078d3cbc11bf2c5298d
Shikhar Jaiswal <shikharjaiswal67@gmail.com>
2019-12-25 22:45:00 +1100

    First

---
+++ venv/Include/aws-test.py
@@ -0,0 +1,11 @@
+import boto3
+
+# s3 = boto3.resource('s3')
+# for bucket in s3.buckets.all():
+#     # print(bucket.name)
+#     if bucket.name:
+#         print("Bucket contains value")
+#     else:
+#         print("Bucket is empty")
+
+kms = 
